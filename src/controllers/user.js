22f365563feaaa37bf3e73a7e1ace09d70de85a3
Will Baxter <bax.will@googlemail.com>
2024-07-17 11:23:51 +0100

    Linter fix

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -59,7 +59,6 @@ export const getAll = async (req, res) => {
 export const updateById = async (req, res) => {
   const { cohort_id: cohortId } = req.body
 
-
   if (!cohortId) {
     return sendDataResponse(res, 400, { cohort_id: 'Cohort ID is required' })
   }
