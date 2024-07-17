d544f56c0bdbd45303e0c13258b312192b3b019e
Angus Townsley <angustownsley@gmail.com>
2024-07-17 11:26:32 +0100

    Update documentation

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -28,7 +28,7 @@ export const create = async (req, res) => {
     const existingUser = await User.findByEmail(userToCreate.email)
 
     if (existingUser) {
-      return sendDataResponse(res, 409, { email: 'Email already in use' })
+      return sendDataResponse(res, 400, { email: 'Email already in use' })
     }
 
     const createdUser = await userToCreate.save()
