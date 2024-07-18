f9cfe7dac233ed54d74a17b3814bbcad1812cbb8
homonoviscoding <mama.leye@gmail.com>
2024-07-18 12:09:53 +0100

    adjusted the cohort domain based on newly added columns

--- src/domain/cohort.js
+++ src/domain/cohort.js
@@ -4,23 +4,30 @@ import dbClient from '../utils/dbClient.js'
  * Create a new Cohort in the database
  * @returns {Cohort}
  */
-export async function createCohort() {
+export async function createCohort(startDate, endDate) {
   const createdCohort = await dbClient.cohort.create({
-    data: {}
+    data: {
+      startDate,
+      endDate
+    }
   })
 
-  return new Cohort(createdCohort.id)
+  return new Cohort(createdCohort)
 }
 
 export class Cohort {
-  constructor(id = null) {
+  constructor(id = null, startDate, endDate) {
     this.id = id
+    this.startDate = startDate
+    this.endDate = endDate
   }
 
   toJSON() {
     return {
       cohort: {
-        id: this.id
+        id: this.id,
+        startDate: this.startDate,
+        endDate: this.endDate
       }
     }
   }
