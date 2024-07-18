c1142c776f216dd94260a86aa4e57447b93455b3
Will Baxter <bax.will@googlemail.com>
2024-07-18 17:00:30 +0100

    Added console log to create (user) function

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -23,6 +23,7 @@ export const create = async (req, res) => {
 
     return sendDataResponse(res, 201, createdUser)
   } catch (error) {
+    console.log(error)
     return sendMessageResponse(res, 500, ERR.UNABLE_TO_CREATE_USER)
   }
 }
