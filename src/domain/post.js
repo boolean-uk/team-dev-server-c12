d7b32d836e6bf5d5e2c63055c3de6ff9514f202d
Periklis <perik76@gmail.com>
2024-07-18 13:46:20 +0300

    more linting errors fixed

--- src/domain/post.js
+++ src/domain/post.js
@@ -3,10 +3,9 @@ import dbClient from '../utils/dbClient.js'
 export default getAllPostsDb = async () => {
   return await dbClient.post.findMany({
     include: {
-          user: true, {
+      user: true,
       select: {
-              profile: true
-          }
+        profile: true
       }
     }
   })
