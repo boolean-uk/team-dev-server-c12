6e461d5dc58f3aa619ad22343487474d2a11fc63
Tim Zoltie <t.zoltie@gmail.com>
2024-07-18 15:19:38 +0100

    updated merge conflict on utils/errors.js

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -4,13 +4,9 @@ export default {
   EMAIL_FORMATING: 'Email format invalid',
   PASSWORD_REQUIREMENTS:
     'Password must contain at least one upper case character, at least one number, at least one special character and not be less than 8 characters in length.',
-<<<<<<< HEAD
   POST_NOT_FOUND: 'Post not found by that Id ',
   USER_NOT_FOUND: 'User not found by that Id',
-  REQUEST_FORBIDDEN: 'User does not have access for request'
-=======
+  REQUEST_FORBIDDEN: 'User does not have access for request',
   EMAIL_PASS_INVALID: 'The email or password is incorrect',
-  POST_NOT_FOUND: 'Post not found by that Id',
   NOT_AUTHORISED: 'You are unauthorised to make that change'
->>>>>>> MrStashy-41-Update_user_patch_endpoint
 }
