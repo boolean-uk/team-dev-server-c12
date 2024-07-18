59602348226411cda1ea91e9b84b26b187e80c09
homonoviscoding <mama.leye@gmail.com>
2024-07-18 14:01:35 +0100

    modified cohort controller to accept a body

--- src/controllers/cohort.js
+++ src/controllers/cohort.js
@@ -2,8 +2,12 @@ import { createCohort } from '../domain/cohort.js'
 import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
+  const { startDate, endDate } = req.body
+  if (!startDate || !endDate) {
+    return sendMessageResponse(res, 400, 'missing fields in request body')
+  }
   try {
-    const createdCohort = await createCohort()
+    const createdCohort = await createCohort(startDate, endDate)
 
     return sendDataResponse(res, 201, createdCohort)
   } catch (e) {
