8db7fa03ae02e8a4fc271578832404d5b664527b
Will Baxter <bax.will@googlemail.com>
2024-07-18 11:31:10 +0100

    Refactored auth login controller, updated errors

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -3,5 +3,6 @@ export default {
   PASSWORD_REQUIRED: 'Password is required',
   EMAIL_FORMATING: 'Email format invalid',
   PASSWORD_REQUIREMENTS:
-    'Password must contain at least one upper case character, at least one number, at least one special character and not be less than 8 characters in length.'
+    'Password must contain at least one upper case character, at least one number, at least one special character and not be less than 8 characters in length.',
+  EMAIL_PASS_INVALID: 'The email or password is incorrect'
 }
