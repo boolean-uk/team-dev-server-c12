d5046bbec316c4e36ada345a94c1cf8d3b8010aa
Will Baxter <bax.will@googlemail.com>
2024-07-19 10:19:35 +0100

    Switched error message order

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -23,7 +23,7 @@ export const create = async (req, res) => {
 
     return sendDataResponse(res, 201, createdUser)
   } catch (error) {
-    console.error(error, ERR.UNABLE_TO_CREATE_USER)
+    console.error(ERR.UNABLE_TO_CREATE_USER, error)
     return sendMessageResponse(res, 500, ERR.UNABLE_TO_CREATE_USER)
   }
 }
