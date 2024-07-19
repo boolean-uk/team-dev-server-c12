871c8f7473bf0b1099e79defb57948cf7c4f9783
Will Baxter <bax.will@googlemail.com>
2024-07-19 10:18:56 +0100

    Added 'error' output to console error

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -23,7 +23,7 @@ export const create = async (req, res) => {
 
     return sendDataResponse(res, 201, createdUser)
   } catch (error) {
-    console.error(ERR.UNABLE_TO_CREATE_USER, 'Unable to create new user')
+    console.error(error, ERR.UNABLE_TO_CREATE_USER)
     return sendMessageResponse(res, 500, ERR.UNABLE_TO_CREATE_USER)
   }
 }
