7c8f1d925c8e5c2921d50a13b0c08bc37e371464
Tim Zoltie <t.zoltie@gmail.com>
2024-07-22 15:28:22 +0100

    latest update

--- tests/api/routes/cohort.spec.js
+++ tests/api/routes/cohort.spec.js
@@ -1,21 +1,22 @@
 import supertest from 'supertest'
 import app from '../../../src/server.js'
-import { createTeacher, createUser } from '../../helpers/createUser.js'
+import { createUser } from '../../helpers/createUser.js'
 import jwt from 'jsonwebtoken'
 
 describe('Cohort Endpoint', () => {
   describe('POST/cohorts', () => {
     it('will allow a user with the role of teacher to create a new cohort', async () => {
-      const teacher = await createTeacher(
+      const teacher = await createUser(
         'teacher@boolean.org',
         'password',
         'TEACHER'
       )
-      const token = jwt.sign(teacher.id, process.env.JWT_SECRET)
+
+      const teacherToken = jwt.sign(teacher.id, process.env.JWT_SECRET)
       const request = {}
       const response = await supertest(app)
         .post('/cohorts')
-        .auth(token, { type: 'bearer' })
+        .auth(teacherToken, { type: 'bearer' })
         .send(request)
 
       expect(response.status).toEqual(201)
--- tests/helpers/createUser.js
+++ tests/helpers/createUser.js
@@ -1,20 +1,11 @@
 import dbClient from '../../src/utils/dbClient'
 import bcrypt from 'bcrypt'
 
-export const createUser = async (email, password) => {
+export const createUser = async (email, password, role) => {
   return await dbClient.user.create({
     data: {
       email: email,
-      passwordHash: await bcrypt.hash(password, 8)
-    }
-  })
-}
-
-export const createTeacher = async (email, password, role) => {
-  return await dbClient.user.create({
-    data: {
-      email: email,
-      passwordHash: await bcrypt.hash(password, 8),
+      password: await bcrypt.hash(password, 8),
       role: role
     }
   })
