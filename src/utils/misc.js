644f194c895ee475e8a78203734a126cfa5306fc
Rafa Lopes <rafalopessecond@gmail.com>
2024-07-25 12:00:26 +0100

    lint file

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid'
 /**
- * @param {Function} callback Optional callback to generate the id 
+ * @param {Function} callback Optional callback to generate the id
  * @defaults uuidV4
  */
 export const generateId = (callback = uuid) => callback()
