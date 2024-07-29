7ab827a40384d5bfec9a562a77aa0c038f2cbd58
Leonardo Lodi <your_email@address>
2024-07-29 12:29:03 +0100

    Feat: add comment model

---
+++ prisma/migrations/20240729111906_add_comment_model/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "content" VARCHAR(255) NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3)
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Comment_postId_key" ON "Comment"("postId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Comment_userId_key" ON "Comment"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
